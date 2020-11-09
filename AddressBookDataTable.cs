c4375844e8ac95ec41abe98246bfc76d35b998fd
MukheshRavi <mukeshparvathi@gmail.com>
2020-11-09 23:14:03 +0530

    [Mukhesh].Add Get contacts By City Order By Name from Data Table

--- AddressBookDataTable.cs
+++ AddressBookDataTable.cs
@@ -86,8 +86,8 @@ namespace AddressBookLINQ
         {
             ArrayList list = new ArrayList ();
             var contact = from c in table.AsEnumerable()
-                               where c.Field<string>("City") == city || c.Field<string>("State") == state
-                               select c;
+                          where c.Field<string>("City") == city || c.Field<string>("State") == state
+                          select c;
             foreach (DataRow dr in contact)
             {
                 Console.WriteLine("\n");
@@ -114,6 +114,31 @@ namespace AddressBookLINQ
                           select c;
 
             Console.WriteLine("Count of contacts in City:{0} and  State:{1} is {2}", city, state, contact.Count());
+        }
+        /// <summary>
+        /// UC 8 Gets all by city.
+        /// </summary>
+        /// <param name="city">The city.</param>
+        public void GetAllByCityOrderByName(string city)
+        {
+            var contact = from c in table.AsEnumerable()
+                          where c.Field<string>("City") == city
+                          orderby c.Field<string>("FirstName"), c.Field<string>("LastName")
+                          select c;
+            foreach (DataRow dr in contact)
+            {
+                Console.WriteLine("\n");
+                Console.WriteLine("FirstName:- " + dr.Field<string>("FirstName"));
+                Console.WriteLine("lastName:- " + dr.Field<string>("LastName"));
+                Console.WriteLine("Address:- " + dr.Field<string>("Address"));
+                Console.WriteLine("City:- " + dr.Field<string>("City"));
+                Console.WriteLine("State:- " + dr.Field<string>("State"));
+                Console.WriteLine("zip:- " + dr.Field<string>("Zip"));
+                Console.WriteLine("phoneNumber:- " + dr.Field<string>("phoneNumber"));
+                Console.WriteLine("eMail:- " + dr.Field<string>("Email"));
+            }
+
+
         }
     }
 }
